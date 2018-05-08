e9aacbeae986c66ce4c1a698076ffa8b5ef16ae0
gan13 <slidyng6@gmail.com>
2018-05-08 11:54:18 +0700

    #5 penambahan Home

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,10 +1,16 @@
+<?php $this->load->view('head')?>
 <div class="row">
-    <div class="col-10">
+    <div class="col-5">
         <h2>Home</h2>
     </div>
 </div>
-    <div class="row">
-        <div class="col-10">
+
+<?php
+    $is_login = $this->session->userdata('is_login');
+    $username = $this->session->userdata('username');
+?>
+    <div class="content">
+        <div class="col-16">
             <p>Selamat datang di Sosial Media Fastikom</p>
             <p>Untuk melihat katalog buku, gunakan menu <strong>"Buku"</strong>.</p>
         </div>
---
+++ application/views/konten.php
@@ -0,0 +1,85 @@
+<section id="team" class="team">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="main_team_area m-y-3">
+                            <div class="head_title center  wow fadeInUp">
+                                <h2>Meet Our Team</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec odio ipsum. Suspendisse
+                                    cursus malesuada facilisis.Lorem ipsum dolor sit amet, consectetur facilisis.</p>
+                            </div>
+
+                            <div class="main_team_content center">
+                                <div class="row">
+                                    <div class="col-md-3">
+                                        <div class="single_team white-text m-t-2 wow zoomIn">
+                                            <img src="assets/img/team1.jpg" alt="team" />
+                                            <div class="single_team_overlay">
+                                                <h4>Mahabubul Islam</h4>
+                                                <p>Html-Coder</p>
+                                                <div class="team_socail">
+                                                    <a href="#!"><i class="fa fa-facebook"></i></a>
+                                                    <a href="#!"><i class="fa fa-twitter"></i></a>
+                                                    <a href="#!"><i class="fa fa-pinterest-p"></i></a>
+                                                    <a href="#!"><i class="fa fa-dribbble"></i></a>
+                                                    <a href="#!"><i class="fa fa-behance"></i></a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <div class="single_team white-text m-t-2 wow zoomIn">
+                                            <img src="assets/img/team2.jpg" alt="team" />
+                                            <div class="single_team_overlay">
+                                                <h4>Mahabubul Islam</h4>
+                                                <p>Html-Coder</p>
+                                                <div class="team_socail">
+                                                    <a href="#!"><i class="fa fa-facebook"></i></a>
+                                                    <a href="#!"><i class="fa fa-twitter"></i></a>
+                                                    <a href="#!"><i class="fa fa-pinterest-p"></i></a>
+                                                    <a href="#!"><i class="fa fa-dribbble"></i></a>
+                                                    <a href="#!"><i class="fa fa-behance"></i></a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <div class="single_team white-text m-t-2 wow zoomIn">
+                                            <img src="assets/img/team3.jpg" alt="team" />
+                                            <div class="single_team_overlay">
+                                                <h4>Mahabubul Islam</h4>
+                                                <p>Html-Coder</p>
+                                                <div class="team_socail">
+                                                    <a href="#!"><i class="fa fa-facebook"></i></a>
+                                                    <a href="#!"><i class="fa fa-twitter"></i></a>
+                                                    <a href="#!"><i class="fa fa-pinterest-p"></i></a>
+                                                    <a href="#!"><i class="fa fa-dribbble"></i></a>
+                                                    <a href="#!"><i class="fa fa-behance"></i></a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <div class="single_team white-text m-t-2 wow zoomIn">
+                                            <img src="assets/img/team4.jpg" alt="team" />
+                                            <div class="single_team_overlay">
+                                                <h4>Mahabubul Islam</h4>
+                                                <p>Html-Coder</p>
+                                                <div class="team_socail">
+                                                    <a href="#!"><i class="fa fa-facebook"></i></a>
+                                                    <a href="#!"><i class="fa fa-twitter"></i></a>
+                                                    <a href="#!"><i class="fa fa-pinterest-p"></i></a>
+                                                    <a href="#!"><i class="fa fa-dribbble"></i></a>
+                                                    <a href="#!"><i class="fa fa-behance"></i></a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div><!-- End of col-sm-3 -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr />
+        </section>
