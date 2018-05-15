7730eda54ad67856e7e30f764381a0c2e2afb9d3
gan13 <slidyng6@gmail.com>
2018-05-15 14:20:32 +0700

    #10 bad

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,18 +1,13 @@
-<?php 
- defined('BASEPATH') OR exit('No direct script access allowed'); 
- class Welcome extends CI_Controller { 
-      function __construct(){ 
-           parent::__construct(); 
-           $this->load->library('encrypt'); 
-      } 
-      function index(){ 
-           $msg = 'Ini Pesan rahasia lho! Jangan bilang siapa-siapa ya!'; //Plain text 
-           $key = '688797800hojnkjvlhvl'; //Key 32 character 
-           //default menggunakan MCRYPT_RIJNDAEL_256 
-           $hasil_enkripsi = $this->encrypt->encode($msg, $key);  
-           $hasil_dekripsi = $this->encrypt->decode($hasil_enkripsi, $key); 
-           echo "Pesan yang mau dienkripsi: ".$msg."<br/><br/>"; 
-           echo "Hasil enkripsi: ".$hasil_enkripsi."<br/><br/>"; 
-           echo "Hasil dekripsi: ".$hasil_dekripsi."<br/><br/>"; 
-      } 
- } 
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+
+class Home extends MY_Controller
+{
+	
+
+	public function index()
+	{
+        
+		$this->load->view('register');
+	}
+}
