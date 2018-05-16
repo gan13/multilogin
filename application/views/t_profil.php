9813941c4a6b3389b9ca4f97e30d5faea9c6beef
gan13 <slidyng6@gmail.com>
2018-05-16 08:09:39 +0700

    add profil

--- application/views/t_profil.php
+++ application/views/t_profil.php
@@ -1072,14 +1072,14 @@
 <!-- ./wrapper -->
 
 <!-- jQuery 2.2.3 -->
-<script src="../../plugins/jQuery/jquery-2.2.3.min.js"></script>
+<script src="<?= base_url('boots/plugins/jQuery/jquery-2.2.3.min.js'); ?>"></script>
 <!-- Bootstrap 3.3.6 -->
-<script src="../../bootstrap/js/bootstrap.min.js"></script>
+<script src="<?= base_url('boots/bootstrap/js/bootstrap.min.js'); ?>"></script>
 <!-- FastClick -->
-<script src="../../plugins/fastclick/fastclick.js"></script>
+<script src="<?= base_url('boots/plugins/fastclick/fastclick.js'); ?>"></script>
 <!-- AdminLTE App -->
-<script src="../../dist/js/app.min.js"></script>
+<script src="<?= base_url('boots/dist/js/app.min.js'); ?>"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="../../dist/js/demo.js"></script>
+<script src="<?= base_url('boots/dist/js/demo.js'); ?>"></script>
 </body>
 </html>
